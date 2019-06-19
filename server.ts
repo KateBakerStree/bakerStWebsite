f318ec80b78dc73dfaba3d477751799bdae1f70a
ben yan <yanbzybzy2@gmail.com>
2019-06-20 00:06:28 +0100

    initial commit, working html project with pure typeScript support

---
+++ server.ts
@@ -0,0 +1,9 @@
+﻿
+var express = require('express');
+var app = express();
+
+//setting middleware
+//app.use(express.static(__dirname + '\public')); //Serves resources from public folder
+app.use(express.static('public'))
+
+var server = app.listen(5000);
---
+++ src/MakeRPC.ts
@@ -0,0 +1,4 @@
+﻿
+var greet: string = "Greetings";
+var geeks: string = "Geeks For Geeks";
+console.log(greet + " from " + geeks); 
