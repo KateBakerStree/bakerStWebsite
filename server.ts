4f7bef95764acfc752535dd14455be63da4e2853
ben yan <yanbzybzy2@gmail.com>
2019-06-21 00:27:05 +0100

    npm run parcel is enough to start a server, get rid of node js launcher

--- server.ts
+++
@@ -1,9 +0,0 @@
-﻿
-var express = require('express');
-var app = express();
-
-//setting middleware
-//app.use(express.static(__dirname + '\public')); //Serves resources from public folder
-app.use(express.static('public'))
-
-var server = app.listen(5000);
